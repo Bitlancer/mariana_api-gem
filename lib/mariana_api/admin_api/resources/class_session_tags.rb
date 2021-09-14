46d41c953efb4d1db66c3f7a1bc12bfb499ee7e9
Jesse Cotton <JCotton1123@gmail.com>
2021-09-14 16:08:00 -0700

    Register endpoint for pulling class session tags

---
+++ lib/mariana_api/admin_api/resources/class_session_tags.rb
@@ -0,0 +1,15 @@
+module MarianaApi::AdminApi::Resources
+  class ClassSessionTags
+    def initialize(http_client)
+      @http_client = http_client
+    end
+
+    def list(params = {})
+      @http_client.get('/api/class_session_tags', params: params).force
+    end
+
+    def read(id, params = {})
+      @http_client.get("/api/class_session_tags/#{id}", params: params)
+    end
+  end
+end
