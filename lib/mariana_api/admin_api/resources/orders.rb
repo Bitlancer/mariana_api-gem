f79a12bfddb9d003c72fedd822d3b37dddc656bf
Matt Juszczak <matt@atopia.net>
2024-06-03 18:51:09 -0500

    add support for classrooms.list and orders.list

---
+++ lib/mariana_api/admin_api/resources/orders.rb
@@ -0,0 +1,15 @@
+module MarianaApi::AdminApi::Resources
+  class Orders
+    def initialize(http_client)
+      @http_client = http_client
+    end
+
+    def list(params = {})
+      @http_client.get('/api/orders', params: params).force
+    end
+
+    def read(id, params = {})
+      @http_client.get("/api/orders/#{id}", params: params)
+    end
+  end
+end
