edef16d65b7ff8396e8e799ae1285c8fb257e83d
Jesse Cotton <jcotton1123@gmail.com>
2026-01-27 13:16:28 -0800

    Add appointments bookings resource (#5)
    * Add appointments bookings resource
    * Add appointments services resource
    * Add guard to fix inline relationship embedding

---
+++ lib/mariana_api/admin_api/resources/appointments_services.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module MarianaApi
+  module AdminApi
+    module Resources
+      class AppointmentsServices
+        def initialize(http_client)
+          @http_client = http_client
+        end
+
+        def list(params = {})
+          params[:per_page] = 25 unless params.key?(:per_page)
+          @http_client.get('/api/appointments/services', params: params)
+        end
+
+        def read(id, params = {})
+          @http_client.get("/api/appointments/services/#{id}", params: params)
+        end
+      end
+    end
+  end
+end
